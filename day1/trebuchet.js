0b2d896b1d0939613a64923c570f1050f13b6fff
Bryan Gomes <bryanpgomes@gmail.com>
2023-12-01 21:23:42 -0500

    I think I need more if statements

--- day1/trebuchet.js
+++ day1/trebuchet.js
@@ -32,7 +32,67 @@ fs.readFile('./puzzleInput.txt', 'utf8', (e,d)=>{
         // should still keep the same loop, but check the next indexs of that singleLineOfText
         let firstNumber = 0;
         let lastNumber = 0;
-       
+       for(let i = 0; i < singleLineOfText.length; i ++){
+        let threeLetterNumber = singleLineOfText[i]+singleLineOfText[i + 1]+singleLineOfText[i + 2]
+        let fourLetterNumber = singleLineOfText[i]+singleLineOfText[i + 1]+singleLineOfText[i + 2]+singleLineOfText[i + 3]
+        let fiveLetterNumber = singleLineOfText[i]+singleLineOfText[i + 1]+singleLineOfText[i + 2]+singleLineOfText[i + 3]+singleLineOfText[i + 4]
+        if(firstNumber === 0){
+            if(threeLetterNumber=== "one"){
+                firstNumber = 1;
+            }
+            if(threeLetterNumber=== "two"){
+                firstNumber = 2;
+            }
+            if(threeLetterNumber=== "six"){
+                firstNumber = 6;
+            }
+            if(fourLetterNumber=== "four"){
+                firstNumber = 4;
+            }
+            if(fourLetterNumber=== "five"){
+                firstNumber = 5;
+            }
+            if(fourLetterNumber=== "nine"){
+                firstNumber = 9;
+            }
+            if(fiveLetterNumber=== "three"){
+                firstNumber = 3;
+            }
+            if(fiveLetterNumber=== "seven"){
+                firstNumber = 7;
+            }
+            if(fiveLetterNumber=== "eight"){
+                firstNumber = 8;
+            }
+        }
+        if(threeLetterNumber=== "one"){
+            firstNumber = 1;
+        }
+        if(threeLetterNumber=== "two"){
+            firstNumber = 2;
+        }
+        if(threeLetterNumber=== "six"){
+            firstNumber = 6;
+        }
+        if(fourLetterNumber=== "four"){
+            firstNumber = 4;
+        }
+        if(fourLetterNumber=== "five"){
+            firstNumber = 5;
+        }
+        if(fourLetterNumber=== "nine"){
+            firstNumber = 9;
+        }
+        if(fiveLetterNumber=== "three"){
+            firstNumber = 3;
+        }
+        if(fiveLetterNumber=== "seven"){
+            firstNumber = 7;
+        }
+        if(fiveLetterNumber=== "eight"){
+            firstNumber = 8;
+        }
+       }
         let calibrationValue = firstNumber + lastNumber
         totalValue += Number(calibrationValue)
     }
